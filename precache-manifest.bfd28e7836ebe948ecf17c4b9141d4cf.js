c1d0d2fba922e817fe76d6b9de601a9b0bfe996d
Abdul Wahhab Khan <abdul.webequator.com>
2020-08-05 19:39:43 +0100

    Updates

---
+++ precache-manifest.bfd28e7836ebe948ecf17c4b9141d4cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0858a27dbd6dc6a0f5185f3681044dad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b8208dd42b6a8def03f",
+    "url": "./static/css/2.e2da7265.chunk.css"
+  },
+  {
+    "revision": "035829adb10cdbd1999c",
+    "url": "./static/css/main.45855d96.chunk.css"
+  },
+  {
+    "revision": "9b8208dd42b6a8def03f",
+    "url": "./static/js/2.3b8264de.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.3b8264de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035829adb10cdbd1999c",
+    "url": "./static/js/main.54f62e8f.chunk.js"
+  },
+  {
+    "revision": "bb6d614b9c9340d72db3",
+    "url": "./static/js/runtime-main.0a137c4e.js"
+  }
+]);
