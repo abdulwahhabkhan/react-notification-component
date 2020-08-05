5bf979712e17b85c44339c7b5e1c43043212ec92
Abdul Wahhab Khan <abdul.webequator.com>
2020-08-05 19:44:45 +0100

    Updates

---
+++ precache-manifest.a26e283b2f206aa708a1386fb2d8a527.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0feec2d7c6294be2591e71fb55ae63ae",
+    "url": "/react-notification-modal/index.html"
+  },
+  {
+    "revision": "9b8208dd42b6a8def03f",
+    "url": "/react-notification-modal/static/css/2.e2da7265.chunk.css"
+  },
+  {
+    "revision": "035829adb10cdbd1999c",
+    "url": "/react-notification-modal/static/css/main.45855d96.chunk.css"
+  },
+  {
+    "revision": "9b8208dd42b6a8def03f",
+    "url": "/react-notification-modal/static/js/2.3b8264de.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-notification-modal/static/js/2.3b8264de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035829adb10cdbd1999c",
+    "url": "/react-notification-modal/static/js/main.54f62e8f.chunk.js"
+  },
+  {
+    "revision": "e49f188a1d56ec81b35d",
+    "url": "/react-notification-modal/static/js/runtime-main.b9b5ec69.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bfd28e7836ebe948ecf17c4b9141d4cf.js"
+  "/react-notification-modal/precache-manifest.a26e283b2f206aa708a1386fb2d8a527.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-notification-modal/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
